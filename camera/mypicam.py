ec68aebab39dd745d92799a3e5f9d73998ecae34
Nate Walck <n8@dropbox.com>
2014-12-11 22:12:48 -0800

    Upped the resolution

--- camera/mypicam.py
+++ camera/mypicam.py
@@ -15,6 +15,7 @@ def take_picture():
 
     with picamera.PiCamera() as camera:
         camera.vflip = True
+        camera.resolution = (1280, 960)
         camera.start_preview()
         time.sleep(2)
         camera.capture(output_file)
